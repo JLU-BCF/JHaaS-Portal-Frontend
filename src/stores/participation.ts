bee2540df92062ebc31da8596f632383504d6f1d
Nils Mittler <nils.mittler@computational.bio.uni-giessen.de>
2023-06-13 16:23:25 +0200

    add ui to participate on hubs

--- src/stores/participation.ts
+++ src/stores/participation.ts
@@ -2,6 +2,14 @@ import { defineStore } from 'pinia';
 import { useNotificationStore } from '@/stores/notification';
 import { fetchWrapper } from '@/helpers/fetch-wrapper';
 
+export type HubForParticipation = {
+  name: string,
+  slug: string,
+  description?: string,
+  startDate: Date,
+  endDate: Date
+}
+
 export const useParticipationStore = defineStore('participation', () => {
   const backend = import.meta.env.VITE_BACKEND_PATH;
   const { notify } = useNotificationStore();
@@ -9,6 +17,18 @@ export const useParticipationStore = defineStore('participation', () => {
   async function fetchUserParticipations() {
     return fetchWrapper
       .get(`${backend}/participation/list`)
+      .then((data) => data)
+      .catch((err) => {
+        notify({
+          display: 'danger',
+          message: err
+        });
+      });
+  }
+
+  async function fetchParticipation(slug: string | string[]) {
+    return fetchWrapper
+      .get(`${backend}/participation/detail/${slug}`)
       .then((data) => data)
       .catch((err) => {
         notify({
@@ -30,5 +50,17 @@ export const useParticipationStore = defineStore('participation', () => {
       });
   }
 
-  return { fetchUserParticipations, fetchJupyterForParticipation };
+  async function requestAccess(slug: string | string[]) {
+    return fetchWrapper
+      .post(`${backend}/participation/hub/${slug}`)
+      .then((data) => data)
+      .catch((err) => {
+        notify({
+          display: 'danger',
+          message: err
+        });
+      });
+  }
+
+  return { fetchUserParticipations, fetchParticipation, fetchJupyterForParticipation , requestAccess};
 });
--- src/views/participation/ParticipateView.vue
+++ src/views/participation/ParticipateView.vue
@@ -1,26 +1,118 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
-import { useParticipationStore } from '@/stores/participation';
-import type { Jupyter } from '@/models/jupyter.model';
+import router from '@/router';
+import { useRoute, RouterLink } from 'vue-router';
+import { useParticipationStore, type HubForParticipation } from '@/stores/participation';
 import { ref, type Ref } from 'vue';
+import { Participation } from '@/models/participation.model';
+import { useNotificationStore } from '@/stores/notification';
 
 const route = useRoute();
 const slug = route.params.slug;
 
 const participationStore = useParticipationStore();
-const jupyter: Ref<Jupyter | undefined> = ref();
+const hub: Ref<HubForParticipation | undefined> = ref();
+const participation: Ref<Participation | undefined> = ref();
+const loadingHub = ref(true);
+const loadingParticipation = ref(false);
+const { notify } = useNotificationStore();
 
 participationStore
   .fetchJupyterForParticipation(slug)
-  .then((jupyterInstance) => (jupyter.value = jupyterInstance));
+  .then((hubInstance) => {
+    hub.value = hubInstance;
+    loadingParticipation.value = true;
+    participationStore.fetchParticipation(slug)
+      .then((participationInstance) => {
+        if (participationInstance) {
+          participation.value = new Participation(participationInstance);
+          notify({
+            display: 'info',
+            message: 'You already requested access to this hub.'
+          })
+        }
+      })
+      .finally(() => {
+        loadingParticipation.value = false;
+      })
+  })
+  .finally(() => {
+    loadingHub.value = false
+  });
 
+function requestAccess(slug: string) {
+  if (!confirm('Do you really want to request access to this hub?')) {
+    return;
+  }
+  participationStore.requestAccess(slug)
+    .then(() => {
+      notify({
+        display: 'info',
+        message: 'Access has been requested.'
+      });
+      router.push({ name: 'participation-overview' });
+    });
+}
 </script>
 
 <template>
-  <h1>Participation request</h1>
-  <hr />
+  <div v-if="loadingHub">
+    <div class="spinner-grow spinner-grow-sm align-middle" role="status"></div>
+    <span class="mx-3">Loading...</span>
+  </div>
 
-  <p>You are about to submit a participation request for the following course:</p>
+  <div v-else-if="hub">
+    <h1>{{ hub.name }}</h1>
+    <div class="card mt-5 mw-330">
+      <div class="card-body">
+        <h6 class="card-title">Description</h6>
+        <p class="card-text">{{ hub.description ?? 'No description provided.' }}</p>
+        <p>
+          <strong>Start Date:</strong> {{ new Date(hub.startDate).toDateString() }}
+          <br>
+          <strong>End Date:</strong> {{ new Date(hub.endDate).toDateString() }}
+        </p>
 
-  {{ jupyter }}
+
+        <div v-if="loadingParticipation">
+          <div class="spinner-grow spinner-grow-sm align-middle" role="status"></div>
+          <span class="mx-3">Loading...</span>
+        </div>
+        <div v-else-if="participation">
+          <h5 class="mt-5">
+            Note:
+          </h5>
+          <p class="mb-0">
+            You already requested participation for this hub:
+            <span v-if="participation.status == 'ACCEPTED'" class="text-success">
+              your request has been accepted!
+            </span>
+            <span v-else-if="participation.status == 'REJECTED'" class="text-danger">
+              your request has been rejected!
+            </span>
+            <span v-else class="text-secondary">
+              your request is pending!
+            </span>
+          </p>
+        </div>
+
+        <div v-else>
+          <button @click="requestAccess(hub.slug)" class="btn btn-success w-100 mt-3">
+            Request access now
+          </button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <div v-else>
+    <p>Oops - something went wrong.</p>
+  </div>
+
+  <RouterLink
+    class="btn btn-outline-dark mt-5 w-100 mw-330"
+    :to="{ name: 'participation-overview' }"
+  >
+    Back
+  </RouterLink>
 </template>
